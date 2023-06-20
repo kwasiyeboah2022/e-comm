0a9ac0a46d3b74aaffbedda7aac72d3493dc4208
kwasiyeboah2022 <akuamoahyaw2015@gmail.com>
2023-06-20 13:53:43 -0700

    toggle values

--- src/index.js
+++ src/index.js
@@ -11,12 +11,26 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+//dev-o23tea21jdb8fvww.us.auth0.com
+
+//eZzYX8iwB4sQAavhKlQQtsc37UVT7Al3
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-o23tea21jdb8fvww.us.auth0.com"
+    clientId="eZzYX8iwB4sQAavhKlQQtsc37UVT7Al3"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 )
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -5,9 +5,9 @@ import {
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
 } from '../actions'
+import { useCartContext } from '../context/cart_context'
 
 const cart_reducer = (state, action) => {
-  console.log(state)
   if (action.type === ADD_TO_CART) {
     const { id, color, amount, product } = action.payload
     console.log(action.payload)
@@ -30,8 +30,9 @@ const cart_reducer = (state, action) => {
         id: id + color,
         name: product.name,
         color,
-        amount,
         price: product.price,
+        amount,
+        max: product.stock,
         image: product.images[0].url,
       }
       return { ...state, cart: [...state.cart, newItem] }
@@ -68,8 +69,25 @@ const cart_reducer = (state, action) => {
       } else {
         return item
       }
+      return { ...state, cart: tempCart }
     })
     return { ...state, cart: tempCart }
+  }
+
+  if (action.type === COUNT_CART_TOTALS) {
+    const { total_items, total_amount } = state.cart.reduce(
+      (total, cartItem) => {
+        const { amount, price } = cartItem
+        total.total_items += amount
+        total.total_amount += price * amount
+        return total
+      },
+      {
+        total_items: 0,
+        total_amount: 0,
+      }
+    )
+    return { ...state, total_items, total_amount }
   }
 
   throw new Error(`No Matching "${action.type}" - action type`)
